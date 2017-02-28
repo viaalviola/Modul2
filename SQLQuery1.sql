5a89240c6acdb520c5d4084de37fe644e1433e19
user <user@DESKTOP-VEBC33S>
2017-02-28 07:32:13 +0700

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO SYSUser (LoginName,PasswordEncryptedText, RowCreatedSYSUserID, RowModifiedSYSUserID) 
+VALUES ('Admin','Admin',1,1) 
+INSERT INTO SYSUserProfile (SYSUserID,FirstName,LastName,Gender,RowCreatedSYSUserID, RowModifiedSYSUserID) 
+VALUES (2,'Vinz','Durano','M',1,1) 
+INSERT INTO SYSUserRole (SYSUserID,LOOKUPRoleID,IsActive,RowCreatedSYSUserID, RowModifiedSYSUserID) 
+VALUES (2,1,1,1,1)
---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+namespace Asp_mvc_2.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
